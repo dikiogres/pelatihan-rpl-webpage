47fc536a564cce9c36078dd2585f7e6a54c284b2
dzikrisyairozi <dikiogres@gmail.com>
2023-08-26 00:00:00 +0700

    feat: route on main

--- main.go
+++ main.go
@@ -2,7 +2,18 @@ package main
 
 import (
 	"os"
+
+	"rst-backend/database"
+
+	middleware "rst-backend/middleware"
+	routes "rst-backend/routes"
+
+	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func main() {
 	port := os.Getenv("PORT")
@@ -13,4 +24,15 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
 }
