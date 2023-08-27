e8161ed84a9818745b289112f75b60c362d2bda8
dzikrisyairozi <dikiogres@gmail.com>
2023-08-28 00:00:00 +0700

    feat: food controller functions

---
+++ controllers/foodController.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"math"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
